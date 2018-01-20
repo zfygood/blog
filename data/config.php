17ee19e55382d96ba48d3011be1852e6cdd0228a
zfygood <zfy427@gmail.com>
2018-01-20 14:23:05 +0800

    Revert "Initial commit"
    This reverts commit d35097f401e9feee4ef308f90b75dcdd25b4a013.

--- data/config.php
+++
@@ -1,6 +0,0 @@
-<?php
-
-return array(
-    'article_path' => APP_PATH.'admin/view/Article/article.html',
-    'article_edit' => APP_PATH.'admin/view/Article/article_edit.html',
-);
--- route/route.php
+++
@@ -1,9 +0,0 @@
-<?php
-
-Route::get('index/index','index/Index/index');
-
-Route::get('article','index/Articles/index');
-
-Route::get('admin/index','admin/Index/index');
-
-Route::get('user','admin/login/login');
